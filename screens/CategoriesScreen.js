9fa5a6e5ce84e86d0ee2f1b7a2ab88f3a864a842
Ramazan KORKMAZ <rakodev@gmail.com>
2019-10-31 16:38:51 +0100

    Add header

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,20 +1,42 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity
+} from "react-native";
 
+import Colors from "../constants/Colors";
 import { CATEGORIES } from "../data/dummy-data";
 
-const renderGridItem = itemData => {
-  return (
-    <View style={styles.gridItem}>
-      <Text>{itemData.item.title}</Text>
-    </View>
-  );
-};
-
 const CategoriesScreen = props => {
+  const renderGridItem = itemData => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() => {
+          props.navigation.navigate({ routeName: "CategoryMeals" });
+        }}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   );
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: ""
+  },
+  headerTintColor: Colors.primaryColor
 };
 
 const styles = StyleSheet.create({
