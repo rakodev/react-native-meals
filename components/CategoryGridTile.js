26c76c0fa4122d5c4841d7de711576a5b6456a08
Ramazan KORKMAZ <rakodev@gmail.com>
2019-11-01 16:16:51 +0100

    Fix CSS Shadow

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -36,7 +36,11 @@ const styles = StyleSheet.create({
     margin: 5,
     height: 150,
     borderRadius: 10,
-    overflow: "hidden"
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21
+        ? "hidden"
+        : "visible",
+    elevation: 5
   },
   container: {
     flex: 1,
@@ -44,8 +48,7 @@ const styles = StyleSheet.create({
     shadowColor: "black",
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 10,
-    elevation: 3,
+    shadowRadius: 5,
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end"
